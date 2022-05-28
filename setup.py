9e59efb22e310c5f90f02e07ec44970bbf163250
Dmitry Zhukov <zhukovdm@proton.me>
2022-05-28 21:50:09 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+from setuptools import setup
+
+
+setup(
+    name="optio",
+    version="0.0.0",
+    description="Python library for parsing command line arguments",
+    packages=[ "optio" ]
+)
