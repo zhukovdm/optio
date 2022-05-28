08d1814f09985b8f5dfb28133a751667262e7f9d
Dmitry Zhukov <zhukovdm@proton.me>
2022-05-29 01:43:58 +0200

    add build info

--- setup.py
+++ setup.py
@@ -4,9 +4,4 @@
 from setuptools import setup
 
 
-setup(
-    name="optio",
-    version="0.0.0",
-    description="Python library for parsing command line arguments",
-    packages=[ "optio" ]
-)
+setup()
